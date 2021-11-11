e4281a74ec1695f15ced3569cb36c897b9033055
Łukasz Adamczyk <lukasz012798@gmail.com>
2021-11-11 22:01:42 +0100

    Play sound

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ class DrumKit {
     this.snareAudio = document.querySelector(".snare-sound");
     this.hihatAudio = document.querySelector(".hihat-sound");
     this.index = 0;
-    this.bpm = 50;
+    this.bpm = 300;
   }
 
   activePad() {
@@ -16,7 +16,21 @@ class DrumKit {
   repeat() {
     let step = this.index % 8;
     const activeBars = document.querySelectorAll(`.b${step}`);
-    console.log(activeBars);
+    activeBars.forEach(bar => {
+      bar.style.animation = `playTrack 0.3s alternate ease-in-out 2`;
+      if (bar.classList.contains("active")) {
+        if (bar.classList.contains("kick-pad")) {
+          this.kickAudio.currentTime = 0;
+          this.kickAudio.play();
+        } else if (bar.classList.contains("snare-pad")) {
+          this.snareAudio.currentTime = 0;
+          this.snareAudio.play();
+        } else if (bar.classList.contains("hihat-pad")) {
+          this.hihatAudio.currentTime = 0;
+          this.hihatAudio.play();
+        }
+      }
+    });
     this.index++;
   }
 
@@ -32,6 +46,9 @@ const drumKit = new DrumKit();
 
 drumKit.pads.forEach(pad => {
   pad.addEventListener("click", drumKit.activePad);
+  pad.addEventListener("animationend", function () {
+    this.style.animation = "";
+  });
 });
 
 drumKit.playBtn.addEventListener("click", () => drumKit.start());
