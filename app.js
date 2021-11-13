173c9de97c4b2ad563b734477e39e10b1b1a74ed
Łukasz Adamczyk <lukasz012798@gmail.com>
2021-11-13 22:32:46 +0100

    Add tempo

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ class DrumKit {
     this.isPlaying = null;
     this.selects = document.querySelectorAll("select");
     this.muteBtns = document.querySelectorAll(".mute");
+    this.tempoSlider = document.querySelector(".tempo-slider");
   }
 
   activePad() {
@@ -55,6 +56,7 @@ class DrumKit {
   updateBtn() {
     if (this.isPlaying) this.playBtn.textContent = "Stop";
     else this.playBtn.textContent = "Play";
+    this.playBtn.classList.toggle("active");
   }
 
   changeSound(e) {
@@ -104,6 +106,21 @@ class DrumKit {
       }
     }
   }
+
+  changeTempo(e) {
+    const tempoText = document.querySelector(".tempo-nr");
+    this.bpm = e.target.value;
+    tempoText.innerText = e.target.value;
+  }
+
+  updateTempo() {
+    clearInterval(this.isPlaying);
+    this.isPlaying = null;
+    const playBtn = document.querySelector(".play");
+    if (playBtn.classList.contains("active")) {
+      this.start();
+    }
+  }
 }
 
 const drumKit = new DrumKit();
@@ -130,4 +147,12 @@ drumKit.muteBtns.forEach(btn => {
   btn.addEventListener("click", function (e) {
     drumKit.mute(e);
   });
+});
+
+drumKit.tempoSlider.addEventListener("input", function (e) {
+  drumKit.changeTempo(e);
+});
+
+drumKit.tempoSlider.addEventListener("change", function (e) {
+  drumKit.updateTempo();
 });
